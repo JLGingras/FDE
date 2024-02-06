7231fc4d7458f2404d393979bedde530a0aab2db
SlimeAlex02 <159090358+SlimeAlex02@users.noreply.github.com>
2024-02-05 21:13:49 -0500

    SpiderEgg
    Added a spider egg

--- src/main/java/net/mcreator/fde/init/FdeModItems.java
+++ src/main/java/net/mcreator/fde/init/FdeModItems.java
@@ -14,6 +14,7 @@ import net.minecraft.world.item.BlockItem;
 
 import net.mcreator.fde.item.YummyBrainItem;
 import net.mcreator.fde.item.SpiderGlandItem;
+import net.mcreator.fde.item.SpiderEggItem;
 import net.mcreator.fde.item.ShulkerHeadItem;
 import net.mcreator.fde.FdeMod;
 
@@ -22,7 +23,15 @@ public class FdeModItems {
 	public static final RegistryObject<Item> SPIDER_GLAND = REGISTRY.register("spider_gland", () -> new SpiderGlandItem());
 	public static final RegistryObject<Item> INFERNAL_FURNACE = block(FdeModBlocks.INFERNAL_FURNACE);
 	public static final RegistryObject<Item> SHULKER_HEAD = REGISTRY.register("shulker_head", () -> new ShulkerHeadItem());
+<<<<<<< Updated upstream
 	public static final RegistryObject<Item> YUMMY_BRAIN = REGISTRY.register("yummy_brain", () -> new YummyBrainItem());
+=======
+<<<<<<< HEAD
+	public static final RegistryObject<Item> SPIDER_EGG = REGISTRY.register("spider_egg", () -> new SpiderEggItem());
+=======
+	public static final RegistryObject<Item> YUMMY_BRAIN = REGISTRY.register("yummy_brain", () -> new YummyBrainItem());
+>>>>>>> 0d99ce2d20d99db8f3d2730fe09292db27bb1925
+>>>>>>> Stashed changes
 
 	private static RegistryObject<Item> block(RegistryObject<Block> block) {
 		return REGISTRY.register(block.getId().getPath(), () -> new BlockItem(block.get(), new Item.Properties()));
